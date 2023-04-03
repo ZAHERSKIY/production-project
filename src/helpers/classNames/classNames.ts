ef6615179ecf8de9f55b908d105df20b60fc681b
ZAHERSKIY <zahar.kirillov.2013@mail.ru>
2023-04-03 18:48:59 +0300

    init

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,16 @@
+//Record сообщает что в качестве ключе будет использоваться string а в качествк значения boolean или string
+type Mods = Record<string, boolean | string>;
+
+export function classNames(
+  cls: string,
+  mods: Mods,
+  additional: string[]
+): string {
+  return [
+    cls,
+    ...additional,
+    Object.entries(mods)
+      .filter(([className, value]) => Boolean(value))
+      .map(([className]) => className),
+  ].join(' ');
+}
